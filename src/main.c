220e2ada979176966b817655b96b720775e78a70
Thomas Bolton <tbolton96@icloud.com>
2026-07-03 18:10:22 +0100

    move startup code to own file

--- src/main.c
+++ src/main.c
@@ -46,18 +46,3 @@ int main(void) {
 
     return 0;
 }
-
-__attribute__((naked, noreturn)) void _reset(void) {
-    extern long _sbss, _ebss, _sdata, _edata, _sidata;
-    for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
-    for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
-
-    main();
-    while (1) (void) 0;
-}
-
-extern void _estack(void);
-
-__attribute__((section(".vectors"))) void (*const tab[16 + 91])(void) = {
-    _estack, _reset, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, SysTick_Handler
-};
---
+++ src/startup.c
@@ -0,0 +1,16 @@
+__attribute__((naked, noreturn)) void _reset(void) {
+    extern long _sbss, _ebss, _sdata, _edata, _sidata;
+    for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
+    for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+
+    extern int main(void);
+    main();
+    while (1) (void) 0;
+}
+
+extern void _estack(void);
+extern void SysTick_Handler(void);
+
+__attribute__((section(".vectors"))) void (*const tab[16 + 91])(void) = {
+    _estack, _reset, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, SysTick_Handler
+};
